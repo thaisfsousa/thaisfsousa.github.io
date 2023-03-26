2fc7335c9d87e5af75a7e15dc8ca3aa141bb4ad6
thaisfsousa <thaisfsousapsi@gmail.com>
2023-03-26 18:00:48 -0300

    interface

--- App/index.js
+++ App/index.js
@@ -1,29 +1,16 @@
-document.getElementById("1").onclick = function (){changedisplay("projetos");}
-document.getElementById("2").onclick = function (){changedisplay("experiencia");}
-document.getElementById("3").onclick = function (){changedisplay("sobre");}
-document.getElementById("4").onclick = function (){changedisplay("contato");}
-document.getElementById("dev").onclick = function (){changedisplay("experiencia");}
-document.getElementById("back").onclick = function (){window.location.reload();}
-document.getElementById("back1").onclick = function (){window.location.reload();}
-document.getElementById("back2").onclick = function (){window.location.reload();}
-document.getElementById("back3").onclick = function (){window.location.reload();}
-document.getElementById("panic").onclick = function (){window.open("https://thaisfsousa.github.io/dont_panic/");}
+document.getElementById("panic").onclick = function (){window.open("https://thaisfsousa.github.io/dontPanic/");}
+document.getElementById("snake").onclick = function (){window.open("https://thaisfsousa.github.io/snakeGame/index.html");}
 document.getElementById("linkedin").onclick = function (){window.open("https://www.linkedin.com/in/thaisfsousa/");}
 document.getElementById("github").onclick = function (){window.open("https://github.com/thaisfsousa");}
+document.getElementById("1").onclick = function (){changedisplay("thais1");}
+document.getElementById("2").onclick = function (){changedisplay("thais2");}
+document.getElementById("3").onclick = function (){changedisplay("thais3");}
 
 
 function changedisplay(id) {
-    const tabs = document.getElementsByClassName("tabcontent active");
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].className = "tabcontent";
-    }
-    document.getElementById(id).className = "tabcontent active";
+  const tabs = document.getElementsByClassName("show");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].className = "hidden";
+  }
+  document.getElementById(id).className = "show";
 }
-
-const gradient = document.querySelector(".gradient");
-
-function onMouseMove(event) {
-  gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(0,0,0) 0, rgb(99,99,99) 10%)';
-}
-document.addEventListener("mousemove", onMouseMove);
-  
